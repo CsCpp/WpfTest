b46d611ef523a727c7bebadb6c311ccfc6ecf248
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-06-21 09:28:38 +0300

    MVVM

---
+++ WpfTest/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTest.Core;
+
+namespace WpfTest.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+        private object _correntView;
+
+        public object currentView
+        {
+            get { return _correntView; }
+            set { _correntView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+                HomeVM = new HomeViewModel();
+            currentView = HomeVM;
+        }
+    }
+}
