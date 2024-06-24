0f9df8fa4a5582165acb77329b747a314e74d92e
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-06-24 08:44:36 +0300

    Environment.Exit(0);

--- WpfTest/MVVM/ViewModel/MainViewModel.cs
+++ WpfTest/MVVM/ViewModel/MainViewModel.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfTest.Core;
 
 namespace WpfTest.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public RelayCommand ExitApp { get; set; }
         public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
         private object _correntView;
 
         public object currentView
@@ -23,7 +29,21 @@ namespace WpfTest.MVVM.ViewModel
         public MainViewModel()
         {
                 HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
             currentView = HomeVM;
+                HomeViewCommand = new RelayCommand(o => 
+                { 
+                currentView = HomeVM;
+                    });
+
+                DiscoveryViewCommand = new RelayCommand(o =>
+                {
+                    currentView = DiscoveryVM;
+                });
+            ExitApp = new RelayCommand(o =>
+            {
+                Environment.Exit(0);
+            });
         }
     }
 }
